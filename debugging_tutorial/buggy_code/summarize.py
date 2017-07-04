23cb53e964b5c16352eb0e02661401f17dab91b7
Kristian Rother <krother@academis.eu>
2017-07-04 22:07:31 +0200

    reviewed the tutorial

--- debugging_tutorial/buggy_code/summarize.py
+++ debugging_tutorial/buggy_code/summarize.py
@@ -1,38 +1,57 @@
 
 import pandas as pd
 import pylab as plt
+from parse_uniprot import parse
+import os
 
 
-def get_means():
-	homosap = pd.read_csv('output/human_proteome.csv', index_col=0)
-	chimp = pd.read_csv('output/chimp_proteome.csv', index_col=0)
-	banana = pd.read_csv('output/banana_proteome.csv', index_col=0)
+IN_PATH = '../data/'
+OUT_PATH = '../output/'
 
-	means = pd.DataFrame(data = {
-		'human': homosap[homosap.columns[3:]].mean(),
-		'chimp': chimp[chimp.columns[3:]].mean(),
-		'banana': banana[banana.columns[5:]].mean(),
-		})
 
+def get_means(table):
+    ss = table[table.columns[2:]].sum()
+    return ss/ss.sum()
+
+def prepare_df():
+    homosap = pd.read_csv('../output/human_proteome.csv', index_col=0)
+    chimp = pd.read_csv('../output/chimp_proteome.csv', index_col=0)
+    banana = pd.read_csv('../output/banana_proteome.csv', index_col=0)
+
+    means = pd.DataFrame(data = {
+        'human': get_means(homosap),
+        'chimp': get_means(chimp),
+        'banana': get_means(banana),
+        })
+    return means
 
 
 def calc_z(means):
-	z = pd.DataFrame({
-	        'chimp': means['human'] / means['chimp'] - 1.0,
-	        'panda': means['human'] / means['banana'] - 1.0,
-	        })
-	z.plot.bar()
-	plt.title('Proteome diversity')
-	plt.ylabel('ratio of means human/x - 1.0')
-	plt.xlabel('amino acid')
-	plt.savefig('barplot.png')
-	return z
+    z = pd.DataFrame({
+            'chimp': means['human'] / means['chimp'] - 1.0,
+            'banana': means['human'] / means['banana'] - 1.0
+            })
+    z.plot.bar()
+    plt.title('Proteome diversity')
+    plt.ylabel('ratio of means human/x - 1.0')
+    plt.xlabel('amino acid')
+    plt.savefig('barplot.png')
+    return z
 
 
-	
+    
 if __name__ == '__main__':
-    means = get_means()
-    print(means.sum(), end='\n\n')
+
+    organisms = ['human', 'chimp', 'banana']
+
+    for org in organisms:
+        in_fn = "{}/{}.fasta".format(IN_PATH, org)
+        out_fn = "{}/{}_proteome.csv".format(OUT_PATH, org)
+        if not os.path.exists(out_fn):
+            parse(in_fn, out_fn)
+
+    means = prepare_df()
+    print(means.sum())
 
     z = calc_z(means)
     print(z.abs().mean())
--- debugging_tutorial/solution/buglist.md
+++ debugging_tutorial/solution/buglist.md
@@ -13,16 +13,3 @@
 * line 62 is a relic, it does nothing
 * strip() missing in line 33
 * amino acid X not accounted for
-
-
-## summarize.py
-
-* line 18 missing return
-* line 13-15, all indices should be 2
-* line 24-25, panda and banana swapped
-
-## pipeline.py
-
-* line 16 potentially dangerous, logging is better
-* accidental comparison operator
-
